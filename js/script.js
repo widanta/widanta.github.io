56279fce8256d733a1cf421af5b448ea8a6ddcf4
widanta <widanta66@gmail.com>
2021-08-08 13:02:47 +0800

    memindahkan script ke folder js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// script contact form
+const scriptURL = "https://script.google.com/macros/s/AKfycbyrVWW2XxaTjYDGe3LlNBsiXRkB2gxM_5gpjno-kBWcrD9hL-uYJRlCG9T6DR2DHrjj/exec";
+const form = document.forms["Contact-Form"];
+
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //ketika tombol loading
+  //tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      //tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      //tampilkan alert
+      myAlert.classList.toggle("d-none");
+      //reset
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
